cd529d06020e24e81afebedfe97649671d2b0ba5
Ezequiel <ezeq.cruel@gmail.com>
2018-12-14 16:48:48 -0200

    Add files via upload

--- config.lua
+++ config.lua
@@ -1,3 +1,11 @@
 -- define window size
 width = 1265
 height = 950
+font_path = "C:\\Windows\\Fonts\\"
+fonts = {
+	"OpenSans-Regular.ttf", 
+	"NotoSans-Regular.ttf",
+	"arialbd.ttf",
+	}
+ui_font = {"OpenSans-Regular.ttf", 11}
+--ui_font = {"arialbd.ttf", 11}

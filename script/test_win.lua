591dfb6c3a5f2207bf69861606e751899769fa1f
Ezequiel <ezeq.cruel@gmail.com>
2019-10-07 09:33:23 -0300

    Add files via upload

--- script/test_win.lua
+++ script/test_win.lua
@@ -1,8 +1,13 @@
+count = 0
+
 function test_script_win()
-	if not cadzinho.nk_begin("script test", 100,100,100,100) then
-		cadzinho.win_close()
+	cadzinho.nk_layout(20, 2)
+	cadzinho.nk_label("Click count:")
+	if cadzinho.nk_button("Increment") then
+		count = count + 1
+		-- cadzinho.db_print(count)
 	end
-	cadzinho.nk_end()
+	cadzinho.nk_label(count)
 end
 
-cadzinho.win_show("test_script_win")
+cadzinho.win_show("test_script_win", "script test", 200,200,200,200)

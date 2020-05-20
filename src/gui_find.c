f742e1b2206310582e0babdcf7f872e7393a52ca
Ezequiel <ezeq.cruel@gmail.com>
2020-05-20 17:31:27 -0300

    Add files via upload

--- src/gui_find.c
+++ src/gui_find.c
@@ -60,6 +60,47 @@ int txt_ent_find(dxf_node *ent, lua_State *L, char* pat, int *start, int *end){
 	return *start > 0;
 }
 
+char * txt_ent_repl(dxf_node *ent, lua_State *L, char* pat, char* rpl){
+	/* using Lua engine, try to find match pattern in DXF entity text */
+	
+	dxf_node  *x = NULL;
+	luaL_Buffer b;
+	int i;
+	
+	char * new_text = NULL;
+	
+	lua_getglobal(L, "string"); /* get library */
+	lua_getfield(L, 1, "gsub"); /* and function to be called */
+	
+	/* get DXF entity text strings */
+	luaL_buffinit(L, &b); /* init the Lua buffer */
+	for (i = 0; x = dxf_find_attr_i(ent, 3, i); i++){
+		/* first, get the additional text (MTEXT ent) */
+		luaL_addstring(&b, x->value.s_data);
+	}
+	for (i = 0; x = dxf_find_attr_i(ent, 1, i); i++){
+		/* finally, get main text */
+		luaL_addstring(&b, x->value.s_data);
+	}
+	luaL_pushresult(&b); /* finalize string and put on Lua stack */
+	
+	/* using Lua, try to find match pattern in text */
+	lua_pushstring(L, pat);
+	lua_pushstring(L, rpl);
+	if (lua_pcall(L, 3, 2, 0) == LUA_OK){
+		if (lua_isnumber(L, -1)) { /* success */
+			//*end = (int)lua_tonumber(L, -1);
+			new_text = (char *)lua_tostring(L, -2);
+			
+		}
+		lua_pop(L, 2); /* clear Lua stack - pop returned values */
+	}
+	
+	lua_pop(L, 1); /* clear Lua stack - pop library "string" */
+	
+	return new_text;
+}
+
 dxf_node * dwg_find(dxf_drawing *drawing, lua_State *L, char* pat, double rect[4], dxf_node **next){
 	/* try to find match pattern in drawing DXF entities text */
 	
@@ -190,11 +231,15 @@ int gui_find_interactive(gui_obj *gui){
 
 int gui_find_info (gui_obj *gui){
 	static lua_State *L = NULL;
+	static dxf_node *found = NULL;
+	static dxf_node *next = NULL;
 	
 	if (gui->modal != FIND) {
 		if (L) {
 			lua_close(L);
 			L = NULL;
+			next = NULL;
+			found = NULL;
 		}
 		return 0;
 	}
@@ -219,9 +264,9 @@ int gui_find_info (gui_obj *gui){
 	nk_layout_row_dynamic(gui->ctx, 20, 2);
 	if (nk_button_label(gui->ctx, "Next")){
 		//find(L, search, repl); /* ---------------test -------- */
-		static double rect[4];
-		static dxf_node *next = NULL;
-		if(dwg_find(gui->drawing, L, search, rect, &next)){
+		double rect[4];
+		
+		if(found = dwg_find(gui->drawing, L, search, rect, &next)){
 			gui->step_x[0] = rect[0];
 			gui->step_y[0] = rect[1];
 			gui->step_x[1] = rect[2];
@@ -232,6 +277,37 @@ int gui_find_info (gui_obj *gui){
 		else gui->step = 0;
 	}
 	if (nk_button_label(gui->ctx, "Replace")){
+		
+		dxf_node *new_ent = dxf_ent_copy(found, DWG_LIFE); /* copy original entity */
+		if (new_ent){
+			/* get edited text */
+			char *blank = "";
+			char *text = txt_ent_repl(new_ent, L, search, repl);
+			if (text){
+				int len = strlen(text);
+				if (!text) text = blank;
+				int i;
+				dxf_node *x;
+				
+				/* replace the text */
+				if (strcmp(new_ent->obj.name, "MTEXT") == 0) 
+					mtext_change_text (new_ent, text, len, DWG_LIFE);
+				else if (strcmp(new_ent->obj.name, "TEXT") == 0) {
+					for (i = 0; x = dxf_find_attr_i(new_ent, 1, i); i++){
+						/* limit of TEXT entity length */
+						len = (len < DXF_MAX_CHARS - 1)? len : DXF_MAX_CHARS - 1;
+						strncpy(x->value.s_data, text, len);
+						x->value.s_data[len] = 0; /* terminate string */
+					}
+				}
+				new_ent->obj.graphics = dxf_graph_parse(gui->drawing, new_ent, 0, DWG_LIFE);
+				dxf_obj_subst(found, new_ent);
+				
+				/* update undo/redo list */
+				do_add_entry(&gui->list_do, "REPLACE");
+				do_add_item(gui->list_do.current, found, new_ent);
+			}
+		}
 		
 	}
 	if (nk_button_label(gui->ctx, "Selection")){

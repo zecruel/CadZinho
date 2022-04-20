c176606e2851c3621f1da911dff3f2ad078a145e
Ezequiel <ezeq.cruel@gmail.com>
2022-04-20 08:07:29 -0300

    Measure improvements

--- src/gui_measure.c
+++ src/gui_measure.c
@@ -55,10 +55,11 @@ int gui_measure_interactive(gui_obj *gui){
 int gui_measure_info (gui_obj *gui){
 	if (gui->modal == MEASURE) {
 		static char pt_str[64];
-		static char dist_str[64], x_str[64], y_str[64], total_str[64];
-		double distance = 0.0, total = 0.0, x = 0.0, y = 0.0;
+		static char dist_str[64], x_str[64], y_str[64], total_str[64], ang_str[64];
+		static char last_dist_str[64], last_x_str[64], last_y_str[64], last_ang_str[64];
+		double distance = 0.0, total = 0.0, x = 0.0, y = 0.0, angle = 0.0;
 		int i;
-		static int init = 0;
+		static int init = 0, init2 = 0;
 		
 		nk_layout_row_dynamic(gui->ctx, 20, 1);
 		nk_label(gui->ctx, "Measure Distance", NK_TEXT_LEFT);
@@ -71,34 +72,62 @@ int gui_measure_info (gui_obj *gui){
 			x = gui->step_x[gui->step] - gui->step_x[gui->step-1];
 			y = gui->step_y[gui->step] - gui->step_y[gui->step-1];
 			distance = sqrt(x*x + y*y);
+			angle = atan2 (y, x) * 180 / M_PI;
 			
 			/* update strings if has relevant information */
 			if (fabs(distance) > 1e-9){
 				snprintf(dist_str, 63, "Distance: %.9g", distance);
 				snprintf(x_str, 63, "X: %.9g", x);
 				snprintf(y_str, 63, "Y: %.9g", y);
+				snprintf(ang_str, 63, "Angle: %.7g", angle);
 				
-				/* calcule total distance along all points entered */
-				total = 0.0;
-				for (i = 0; i < gui->step; i++){
-					x = gui->step_x[i+1] - gui->step_x[i];
-					y = gui->step_y[i+1] - gui->step_y[i];
-					total += sqrt(x*x + y*y);
+				if (gui->step > 1){
+					/* calcule distance between last previous points */
+					x = gui->step_x[gui->step - 1] - gui->step_x[gui->step - 2];
+					y = gui->step_y[gui->step - 1] - gui->step_y[gui->step - 2];
+					distance = sqrt(x*x + y*y);
+					angle = atan2 (y, x) * 180 / M_PI;
+					snprintf(last_dist_str, 63, "Distance: %.9g", distance);
+					snprintf(last_x_str, 63, "X: %.9g", x);
+					snprintf(last_y_str, 63, "Y: %.9g", y);
+					snprintf(last_ang_str, 63, "Angle: %.7g", angle);
+					
+					/* calcule total distance along all points entered */
+					total = 0.0;
+					for (i = 0; i < gui->step - 1; i++){
+						x = gui->step_x[i+1] - gui->step_x[i];
+						y = gui->step_y[i+1] - gui->step_y[i];
+						total += sqrt(x*x + y*y);
+					}
+					snprintf(pt_str, 63, "Points: %d", gui->step);
+					snprintf(total_str, 63, "Total: %.9g", total);
+					
+					init2 = 1; /* strings updated*/
 				}
-				snprintf(pt_str, 63, "Points: %d", gui->step + 1);
-				snprintf(total_str, 63, "Total: %.9g", total);
-				
 				init = 1; /* strings updated*/
 			}
 		}
 		
 		if (init){ /* show informations */
-			nk_layout_row_dynamic(gui->ctx, 15, 1);
+			nk_layout_row_dynamic(gui->ctx, 13, 1);
+			nk_label_colored(gui->ctx, "Current:", NK_TEXT_LEFT, nk_rgb(255,255,0));
 			nk_label(gui->ctx, dist_str, NK_TEXT_LEFT);
 			nk_label(gui->ctx, x_str, NK_TEXT_LEFT);
 			nk_label(gui->ctx, y_str, NK_TEXT_LEFT);
-			nk_label(gui->ctx, pt_str, NK_TEXT_LEFT);
-			nk_label(gui->ctx, total_str, NK_TEXT_LEFT);
+			nk_label(gui->ctx, ang_str, NK_TEXT_LEFT);
+			nk_layout_row_dynamic(gui->ctx, 5, 1);
+			if (init2){
+				nk_layout_row_dynamic(gui->ctx, 13, 1);
+				nk_label_colored(gui->ctx, "Last:", NK_TEXT_LEFT, nk_rgb(255,255,0));
+				nk_label(gui->ctx, last_dist_str, NK_TEXT_LEFT);
+				nk_label(gui->ctx, last_x_str, NK_TEXT_LEFT);
+				nk_label(gui->ctx, last_y_str, NK_TEXT_LEFT);
+				nk_label(gui->ctx, last_ang_str, NK_TEXT_LEFT);
+				nk_layout_row_dynamic(gui->ctx, 5, 1);
+				nk_layout_row_dynamic(gui->ctx, 13, 1);
+				nk_label(gui->ctx, pt_str, NK_TEXT_LEFT);
+				nk_label(gui->ctx, total_str, NK_TEXT_LEFT);
+			}
 		}
 	}
 	return 1;

2653cc3d8ea98ad0a018da1ddcfb0f513f3b44f0
Ezequiel <ezeq.cruel@gmail.com>
2023-02-01 09:46:18 -0300

    Add files via upload

---
+++ .github/workflows/test.js
@@ -0,0 +1,17 @@
+//npm install --save screenshot-desktop
+
+const exec = require('child_process').execFile;
+const screenshot = require("screenshot-desktop");
+
+const subprocess = exec('cadzinho.exe', function(err, data) {  
+        console.log(err)
+        console.log(data.toString());                       
+    });
+
+setTimeout(() => {
+  screenshot({ filename: 'teste.png' });
+}, 1000);
+
+setTimeout(() => {
+  subprocess.kill(); // Does not terminate the Node.js process in the shell.
+}, 2000);

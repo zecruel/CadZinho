4fa5b3a32ed1d186320e5bc5c9ba78db4ee086fe
Ezequiel <ezeq.cruel@gmail.com>
2025-01-09 13:45:18 -0300

    Add new 3d features

--- interface_3d.lua
+++ interface_3d.lua
@@ -3,6 +3,13 @@ radius = {value = '4'}
 pyr_top_scale = {value = '0'}
 pyr_heigth = {value = '1'}
 slab_heigth = {value = '1'}
+extr_top_scalex = {value = '1'}
+extr_top_scaley = {value = '1'}
+extr_heigth = {value = '1'}
+extr_slices = {value = '0'}
+extr_twist = {value = '0'}
+contour = "{{-0.5,-0.5},{0.5,-0.5},{0.5,-0.4},{-0.4,-0.4},{-0.4,0.5},{-0.5,0.5}}"
+path = "{{0.0,0.0,0.0},{10,10,0.0},{15,10,0.0},\n{25,5,0.0},{25,0.0,0.0}}"
 
 function inter3d_dyn(event)
   cadzinho.nk_layout(20, 1)
@@ -126,6 +133,60 @@ function pyramid_dyn(event)
 	end
 end
 
+function extrude_dyn(event)
+  cadzinho.nk_layout(20, 1)
+  cadzinho.nk_label("New extrude")-- Simple information: script title
+  cadzinho.nk_propertyd("Heigth", extr_heigth)
+  cadzinho.nk_propertyd("Sx", extr_top_scalex, 0)
+  cadzinho.nk_propertyd("Sy", extr_top_scaley, 0)
+  cadzinho.nk_propertyi("Slices", extr_slices, 0)
+  cadzinho.nk_propertyd("Twist", extr_twist, 0)
+	if count == 0 then
+    cadzinho.nk_label("Center point")
+		if event.type == 'enter' then
+			count = count + 1
+			x0 = event.x
+			y0 = event.y
+			
+		elseif event.type == 'cancel' then
+			cadzinho.stop_dynamic()
+		end
+	else
+    cadzinho.nk_label("Confirm")
+		x1 = event.x
+		y1 = event.y
+    z = 0.0
+    p = tonumber(extr_heigth.value)
+    sx = tonumber(extr_top_scalex.value)
+    sy = tonumber(extr_top_scaley.value)
+    slices = tonumber(extr_slices.value)
+    twist = tonumber(extr_twist.value)
+    
+    rot = ""
+    if p < 0 then
+      twist = twist * -1
+      rot = "manifold[1]:mirror(0,0,1)\n" 
+    end
+    
+		
+    extrude = cadzinho.new_mesh("manifold[1] = extrude("..
+        contour ..",".. math.abs(p) ..",".. sx ..",".. sy ..",".. slices ..",".. twist ..")\n" ..
+        rot .. "manifold[1]:translate(" .. x0 ..",".. y0 .. "," .. z ..")")
+    cadzinho.ent_draw(extrude)
+			
+		if event.type == 'enter' then
+      extrude:write()
+			
+      -- back to first point
+			count = 0
+			
+		elseif event.type == 'cancel' then
+			-- back to first point
+			count = 0
+		end
+	end
+end
+
 function slab_dyn(event)
   cadzinho.nk_layout(20, 1)
   cadzinho.nk_label("New slab")-- Simple information: script title
@@ -208,6 +269,15 @@ function inter3d_win()
   if cadzinho.nk_button("slab") then
     count = 0
     cadzinho.start_dynamic("slab_dyn")
+  end
+  if cadzinho.nk_button("Extrude") then
+    count = 0
+    cadzinho.start_dynamic("extrude_dyn")
+  end
+  if cadzinho.nk_button("Extrude_path") then
+    extr = cadzinho.new_mesh("manifold[1] = extrude_path("..
+        contour ..",\n".. path ..", 100)\nmanifold[1]:rotate(0,90)")
+    extr:write()
   end
   if cadzinho.nk_button("rotate") then
     count = 0
